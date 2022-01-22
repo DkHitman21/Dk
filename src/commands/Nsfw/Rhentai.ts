9a63c7a4813626bf321f8cb445a29b6f26c78a47
Dasabae <85019441+iamherok@users.noreply.github.com>
2022-01-22 13:12:31 +0530

    Update Rhentai.ts

--- src/commands/Nsfw/Rhentai.ts
+++ src/commands/Nsfw/Rhentai.ts
@@ -3,6 +3,8 @@ import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
 import axios from 'axios'
+const nHentai = require('shentai')
+const sHentai = new nHentai
 import request from '../../lib/request'
 import { MessageType } from '@adiwajshing/baileys'
 // import { MessageType, Mimetype } from '@adiwajshing/baileys'
@@ -20,9 +22,14 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
+if (!(await this.client.getGroupData(M.from)).nsfw)
+			return void M.reply(
+				`Don't be a pervert, Baka! This is not an NSFW group.`
+			);
+const h = await sHentai.getRandom()
         // fetch result of https://waifu.pics/api/sfw/waifu from the API using axios
-        const { data } = await axios.get('https://api-xcoders.xyz/api/anime/nhentai?apikey=Zl0clXuAbx')
-        const buffer = await request.buffer(data.result.thumbnail).catch((e) => {
+        //const { data } = await axios.get('https://api-xcoders.xyz/api/anime/randomnhentai?apikey=Zl0clXuAbx')
+        const buffer = await request.buffer(h.cover).catch((e) => {
             return void M.reply(e.message)
         })
         while (true) {
@@ -32,17 +39,17 @@ export default class Command extends BaseCommand {
                     MessageType.image,
                     undefined,
                     undefined,
-                    `*Name*: ${data.result.title}\n*Uploaded at*: ${data.result.uploaded_at}\n*Code*: ${data.result.code_nuklir}\n*Artist*: ${data.result.artist.name}\n${data.result.artist.count}\n${data.result.artist.url}\n  `,
-                    undefined
+                    `*Name*: ${h.titles.english}\n*Code*: ${h.id}\n`,
+                     undefined
                 ).catch((e) => {
                     console.log(`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
                     // console.log('Failed')
-                    M.reply(`Could not fetch image. Here's the URL: ${data.url}`)
+                    M.reply(`Could not fetch image. Here's the URL: ${h.cover}`)
                 })
                 break
             } catch (e) {
                 // console.log('Failed2')
-                M.reply(`Could not fetch image. Here's the URL : ${data.url}`)
+                M.reply(`Could not fetch image. Here's the URL : ${h.cover}`)
                 console.log(`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
             }
         }
