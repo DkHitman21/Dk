6457fbb09f76b6e46eb78925d3bcfb6389e2379d
Calvy <90485412+ugsf47@users.noreply.github.com>
2022-01-23 15:30:50 +0200

    Create DkHitman.ts

---
+++ src/commands/General/DkHitman.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "dk",
+			description: "Displays the info",
+			category: "general",
+			usage: `${client.config.prefix}chitoge`,
+			baseXp: 200,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://telegra.ph/file/edc4222f49e35a2b6f643.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `👩🏻‍🦰 *Becky* 👩🏻‍🦰\n\n🍀 *Description: A WhatsApp Bot With Rich Anime Features.*\n\n🌐 *URL: https://github.com/DkHitman21/Dk* \n\n 📒 *Guide: https://github.com/DkHitman21/Dk-Guides* \n`,
+			}
+		);
+	};
+}
