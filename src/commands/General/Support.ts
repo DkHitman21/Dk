bdb0efa750a8493907471f572d5c0617e52b707b
DkHitman21 <96769426+DkHitman21@users.noreply.github.com>
2022-01-24 19:40:54 +0200

    Create Support.ts

---
+++ src/commands/General/Support.ts
@@ -0,0 +1,37 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'support',
+            aliases: ['support'],
+            description: 'Gets the support group links',
+            category: 'general',
+            usage: `${client.config.prefix}Support`,
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        (await this.client.sendMessage(
+        M.sender.jid,
+        `    ♥️MY Lit GROUP♥️
+       *😈 Hitman support:😈* *https://chat.whatsapp.com/JQiAOFypdm04PNBfvPINlh*
+        *😈support😈* INVITE LINK🎉:*https://chat.whatsapp.com/JQiAOFypdm04PNBfvPINlh*`,
+           MessageType.text
+        ))
+        const n = [
+            './assets/videos/BeckyLynch/becky.git.mp4'
+        ]
+        let beckylynch = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url:beckylynch }, MessageType.video, {quoted:M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption: `Sent you the support Link in personal message \n` }
+        )
+
+        }
+}
