e9e6a2278ff2c877b193b24d397dcbc11f45d879
DkHitman21 <96769426+DkHitman21@users.noreply.github.com>
2022-01-24 18:33:41 +0200

    Update Play.ts

--- src/commands/Media/Play.ts
+++ src/commands/Media/Play.ts
@@ -25,14 +25,14 @@ export default class Command extends BaseCommand {
         if (!videos || videos.length <= 0) return void M.reply(`⚓ No Matching videos found for the term : *${term}*`)
         const audio = new YT(videos[0].url, 'audio')
         if (!audio.url) return
-        M.reply('🌟 Sending...')
+        M.reply('💞 wait🎶masaka...')
         this.client
             .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
                 quoted: M.WAMessage,
                 contextInfo: {
                     externalAdReply: {
                         title: videos[0].title.substr(0, 30),
-                        body: `author : ${videos[0].author.name.substr(0, 20)}\n🌟 DkHitman 🌟`,
+                        body: `author : ${videos[0].author.name.substr(0, 20)}\n🎶 DkHitman 🎶`,
                         mediaType: 2,
                         thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`,
                         mediaUrl: audio.url
