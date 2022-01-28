8ee3c2615397ebbd0be80a07395186347acd7fcc
DkHitman21 <96769426+DkHitman21@users.noreply.github.com>
2022-01-28 23:15:02 +0200

    Delete Hitman.ts

--- src/commands/General/Hitman.ts
+++
@@ -1,6 +0,0 @@
-Chitoge
-Dk
-
-
-Are the bot that are of the great bot all of the codes are made by them, if anyone one of you
-this file pls check the bot that I mentioned earlier I am not sure I will able to support this bot anymore after all :)
