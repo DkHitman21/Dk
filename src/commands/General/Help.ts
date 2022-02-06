37a2eaff4f17fec49f2bd587a5df52f3760edc1d
DkHitman21 <96769426+DkHitman21@users.noreply.github.com>
2022-02-06 23:06:28 +0200

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -45,7 +45,7 @@ export default class Command extends BaseCommand {
 			}
 			let text = `👩🏻‍🦰 Hey! *@${
 				user.split("@")[0]
-			}*, 𝐈'𝐦 𝐡𝐢𝐭𝐦𝐚𝐧 𝐚 𝐛𝐨𝐭 𝐛𝐮𝐢𝐥𝐭 𝐭𝐨 𝐦𝐚𝐤𝐞 𝐲𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐞𝐧𝐣𝐨𝐲𝐚𝐛𝐥𝐞❤️.\n\nMy prefix is - "${
+			}*,𝐈'𝐦 𝐡𝐢𝐭𝐦𝐚𝐧 𝐚 𝐛𝐨𝐭 𝐛𝐮𝐢𝐥𝐭 𝐭𝐨 𝐦𝐚𝐤𝐞 𝐲𝐨𝐮𝐫 𝐠𝐫𝐨𝐮𝐩 𝐞𝐧𝐣𝐨𝐲𝐚𝐛𝐥𝐞❤️.\n\nMy prefix is - "${
 				this.client.config.prefix
 			}"\n\n*╚『•My Cmd List•』╝*.\n\n`;
 			const keys = Object.keys(categories);
